56308da645717999ff4d19f80969b744739f6b69
Ania-su <antsaaa.randria@gmail.com>
2025-12-10 07:53:42 +0300

    make dbconnection attribute to final

--- src/main/java/org/example/DBConnection.java
+++ src/main/java/org/example/DBConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private String URL = "jdbc:postgresql://localhost:5432/product_management_db";
-    private String user = "product_manager_user";
-    private String password = "123456";
+    private final String URL = "jdbc:postgresql://localhost:5432/product_management_db";
+    private final String user = "product_manager_user";
+    private final String password = "123456";
 
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, user, password);
